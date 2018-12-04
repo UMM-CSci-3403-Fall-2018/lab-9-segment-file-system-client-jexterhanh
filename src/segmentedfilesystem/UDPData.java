f189d4cf687f49adefae1d96d624612202680ed7
plumstyle <anxxx154@morris.umn.edu>
2018-12-04 17:27:08 -0600

    change the packet number to integer

--- src/segmentedfilesystem/UDPData.java
+++ src/segmentedfilesystem/UDPData.java
@@ -4,16 +4,15 @@ public class UDPData {
 
     private byte status;
     private byte fileID;
-    private byte[] packetNumber = new byte[2];
+    private int packetNumber;
     private byte[] packetData = new byte[124];
     public int dataLength = 0;
 
     public UDPData(byte[] packet) {
         this.status = packet[0];
         this.fileID = packet[1];
-        for (int i = 2; i < 4; i++) {
-            packetNumber[i - 2] = packet[i];
-        }
+        // credit to @Ethan
+        this.packetNumber = (packet[2] << 8) | packet[3] & 0xFF;
         for (int i = 4; i < packet.length; i++) {
             packetData[i - 4] = packet[i];
             dataLength += 1;
@@ -28,11 +27,11 @@ public class UDPData {
         return this.fileID;
     }
 
-    public byte[] getPacketNumber() {
-        return packetNumber;
+    public int getPacketNumber() {
+        return this.packetNumber;
     }
     public byte[] getPacketData() {
-        return packetData;
+        return this.packetData;
     }
 
 }
